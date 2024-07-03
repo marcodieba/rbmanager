d5a64034b7639202e13b9c2de998e20c6184181b
marcodieba <marcodieba@gmail.com>
2024-07-03 09:20:33 -0300

    fix: button financeiro

--- src/core/settings.py
+++ src/core/settings.py
@@ -178,3 +178,9 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
