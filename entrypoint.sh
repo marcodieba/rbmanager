5faf9c2e5a40675f6bb8de15a6fc3ba75ea5f611
Marco <marcodieba@gmail.com>
2024-09-20 13:31:54 -0300

    fix:entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
+# #!/bin/bash
+# set -e
+
+# # Execute as migrações
+# pipenv run python manage.py migrate
+
+# # Colete os arquivos estáticos
+# pipenv run python manage.py collectstatic --noinput
+
+# # Execute o comando passado como argumento (ou padrão do Dockerfile)
+# exec "$@"
+
 #!/bin/bash
-set -e
-
-# Execute as migrações
-pipenv run python manage.py migrate
-
-# Colete os arquivos estáticos
-pipenv run python manage.py collectstatic --noinput
-
 
 # Expande a variável de ambiente PORT e executa o Daphne
 exec daphne -b 0.0.0.0 -p ${PORT:-8000} core.asgi:application
