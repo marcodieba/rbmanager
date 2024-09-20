410214b4e771ac7bdbbf66684d31f23a0b3ec1b5
Marco <marcodieba@gmail.com>
2024-09-20 13:28:05 -0300

    fix:dockfile para railway entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ pipenv run python manage.py migrate
 # Colete os arquivos estáticos
 pipenv run python manage.py collectstatic --noinput
 
-# Execute o comando passado como argumento (ou padrão do Dockerfile)
-exec "$@"
+
+# Expande a variável de ambiente PORT e executa o Daphne
+exec daphne -b 0.0.0.0 -p ${PORT:-8000} core.asgi:application
