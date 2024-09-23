80a87522e9c7a4d80dd7a153429398dbc9aff951
Marco <marcodieba@gmail.com>
2024-09-23 14:34:06 -0300

    fix:abordagem diferente entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,16 @@ set -e
 export PIPENV_VENV_IN_PROJECT=1
 pipenv install --deploy --ignore-pipfile
 
-# Execute as migrações
-pipenv run python manage.py migrate
+# # Execute as migrações
+# pipenv run python manage.py migrate
 
-# Colete os arquivos estáticos
-pipenv run python manage.py collectstatic --noinput
+# # Colete os arquivos estáticos
+# pipenv run python manage.py collectstatic --noinput
+
+if [ "$RUN_COLLECTSTATIC" = "true" ]; then
+    python manage.py collectstatic --noinput
+fi
+
 
 # Expande a variável de ambiente PORT e executa o Daphne
 exec pipenv run daphne -b 0.0.0.0 -p ${PORT:-8080} core.asgi:application
