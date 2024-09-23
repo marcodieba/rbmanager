6e6dc463653442604c67d469be9d195ccc651158
Marco <marcodieba@gmail.com>
2024-09-23 10:25:40 -0300

    fix:entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-# Ativa o ambiente virtual
-pipenv shell
+# Ativa o ambiente virtual do Pipenv
+export PIPENV_VENV_IN_PROJECT=1
+pipenv install --deploy --ignore-pipfile
 
 # Execute as migrações
-# pipenv run python manage.py migrate
+#pipenv run python manage.py migrate
 
 # Colete os arquivos estáticos
 pipenv run python manage.py collectstatic --noinput
 
 # Expande a variável de ambiente PORT e executa o Daphne
-exec daphne -b 0.0.0.0 -p ${PORT:-8080} core.asgi:application
+exec pipenv run daphne -b 0.0.0.0 -p ${PORT:-8080} core.asgi:application
