6915f54f1c97b67c6fbcc6cd62a964f3e95ed1e3
Marco <marcodieba@gmail.com>
2024-09-23 15:13:37 -0300

    fix:reset entrypoint and update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -e
 
-# Ativa o ambiente virtual do Pipenv
-# export PIPENV_VENV_IN_PROJECT=1
-# pipenv install --deploy --ignore-pipfile
-
 # # Execute as migrações
 # pipenv run python manage.py migrate
 
-# # Colete os arquivos estáticos
+# Colete os arquivos estáticos
 # pipenv run python manage.py collectstatic --noinput
 
-# if [ "$RUN_COLLECTSTATIC" = "true" ]; then
-#     python manage.py collectstatic --noinput
-# fi
+# # Execute o comando passado como argumento (ou padrão do Dockerfile)
+# exec "$@"
 
+#!/bin/bash
 
 # Expande a variável de ambiente PORT e executa o Daphne
-exec pipenv run daphne -b 0.0.0.0 -p ${PORT:-8080} core.asgi:application
+exec daphne -b 0.0.0.0 -p ${PORT:-8080} core.asgi:application
