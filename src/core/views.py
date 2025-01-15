2d47339e213c662d1c380b5179bc286d1c559dad
marcodieba <marcodieba@gmail.com>
2025-01-15 16:19:35 -0300

    fix:viwes

--- src/core/views.py
+++ src/core/views.py
@@ -45,8 +45,7 @@ def controle(request):
     
     vendas_mes = 0
     vendas_ano = 0
-    mes = Financeiro.objects.filter(data__year=ano).aggregate(Sum('saida'))
-    print(mes)
+    mes = Financeiro.objects.filter(data__year=ano)
     gastos = 0
     mortes = 0
 
@@ -88,11 +87,11 @@ def controle(request):
     data_nascimento_10 = ''
     data_nascimento_11 = ''
     data_nascimento_12 = ''
-    # for obj in mes:
-    #     # total_entrada = Financeiro.objects.aggregate(entrada=Sum('entrada'))
-    #     gastos = float(gastos) + float(obj.saida)
-    # # gastos = locale.currency(gastos, grouping=True ) valor como moeda
-    # gastos = gastos
+    for obj in mes:
+        # total_entrada = Financeiro.objects.aggregate(entrada=Sum('entrada'))
+        gastos = float(gastos) + float(obj.saida)
+    # gastos = locale.currency(gastos, grouping=True ) valor como moeda
+    gastos = gastos
     # print(gasto)
     for venda in animais:
         if venda.tipo_movimento_id == 1:
@@ -180,8 +179,8 @@ def controle(request):
     grafico_data =['Jan', 'Fev', 'Mar', 'Mai', 'Abr', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
     context = {
                 # 'total_entrada':total_entrada,
-                # 'gastos': gastos,
-                'mes':mes,
+                'gastos': gastos,
+                # 'mes':mes,
                 'vendas_ano':vendas_ano,
                 'vendas_mes':vendas_mes,
                 'fazenda_nome':fazenda_nome,
